610d431a43590e3c336d101f7c5ca88ca4ad1923
kossanr <58185702+kossanr@users.noreply.github.com>
2020-08-19 11:17:03 -0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const zipAPIKEY =
-  "duPFbzHQfFWaRSOJACn7sh9sx0GKQQGDamCtW9nylUU1m8YAdZKTHTjRRTS8syCt";
+  "js-qSvR4eXAk9EwtmBg3atyhrcVCc0l282T7mFTuQjGGXnc0kf9SWanc7ndzgLVvXDk";
 
+/* zipcodeapi.com app code - "VIwRHliTEf4e1Jys4hIIqlolFlOYeqf13UBd1oPcFtHvk2bKkqYO3P5O7wfqn5Xh"; */
 const birdAPIKey = "r0vtns5mffqv";
 
 function watchZipForm() {
@@ -11,44 +12,73 @@ function watchZipForm() {
     if (zipCode !== "") {
       //if the zipCode is not blank
       const zipURL = `https://www.zipcodeapi.com/rest/${zipAPIKEY}/info.json/${zipCode}/degrees`;
-    //get URL that contains my API key and val of #zip-code
+      //get URL that contains my API key and val of #zip-code
       fetch(zipURL)
         .then((res) => res.json()) //formats into json
         .then((res) => {
-          console.log(res);
-        }
-
-          //gather lat and long from zip code data
-          // res.lat and res.lng. Create variables or an object with these? 
-          const zipLat = res.items.lat;
-
-          //pass same lat and long data into ebird URL
-          //fetch that specific URL, format into JSON data
-          //iterate through data and display birds that match such lat & long onto DOM
-
-
-
-
-function getBirds() {
-
-          const birdURL = `https://api.ebird.org/v2/data/obs/geo/recent?lat={{lat}}&lng={{lng}}`;
-
-          var myHeaders = new Headers();
-          myHeaders.append("X-eBirdApiToken", birdAPIKey);
-
-          let options = {headers:myHeaders}
-          
-          fetch(birdAPIURL,options).then(res=>res.json()).then(res=>{
-          if (i = 0; i < res.data.length; i++) {
-            //append to "results" in HTML 
-
-
-          }
-          //})
+          getBirds(res);
         })
-        .catch((err) => console.error({ err }));
-    }}
+        .catch((err) => alert("Please try again later"));
+    } else {
+      alert("Please enter Zip Code");
+    }
+
+    //gather lat and long from zip code data
+    // res.lat and res.lng. Create variables or an object with these?
+    //or just pass  lat and long data into ebird URL
+    //fetch that specific URL, format into JSON data
   });
+}
+
+function getBirds(res) {
+  const birdAPIURL = `https://api.ebird.org/v2/data/obs/geo/recent?lat=${res.lat}&lng=${res.lng}`;
+
+  var myHeaders = new Headers();
+  myHeaders.append("X-eBirdApiToken", birdAPIKey);
+
+  let options = {
+    headers: myHeaders,
+  };
+
+  fetch(birdAPIURL, options)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error(res.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson, res.city, res.state))
+    .catch((err) => {
+      $(".error-message").text(`Something went wrong: ${err}`);
+    });
+}
+
+function displayResults(responseJson, city, state) {
+  console.log(responseJson);
+
+  $(".results-list").html("");
+  $(".num-of-results").text(responseJson.length);
+
+  $(".results").removeClass("hidden");
+
+  $(".results h2").text(`Results for ${city},${state}`);
+
+  for (i = 0; i < responseJson.length; i++) {
+    let html = `<li class="bird-sighting"><span class="name">${
+      responseJson[i].comName
+    }</span><span class="location">${
+      responseJson[i].locName.split("--")[0]
+    }</span>`;
+
+    if (responseJson[i].locName.split("--")[1] !== undefined) {
+      html += `<span class="sub-location">${
+        responseJson[i].locName.split("--")[1]
+      }</span>`;
+    }
+
+    html += "</li>";
+    $(".results-list").append(html);
+  }
 }
 
 $(watchZipForm);
